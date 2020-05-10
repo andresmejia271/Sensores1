afe3dcc27daa29d7e399d35eb1328b2b5c51d70b
andres mejia <andresfmejia2009@gmail.com>
2020-05-10 14:09:49 -0500

    SQL

---
+++ BASEDEDATOSmod.sql
@@ -0,0 +1,76 @@
+
+USE AndresMejia
+
+CREATE TABLE audicion
+(
+	edad INT NULL,
+	audifonos INT NULL,
+    	frecuencia INT NULL,
+		horas INT NULL,
+		traumas INT NULL,
+		necesidad INT NULL,
+		repetir INT NULL,
+		ruidofondo INT NULL,
+		timbre INT NULL,
+		entornosruidosos INT NULL,
+		hearingteste INT NULL,
+	segundotest INT NULL,
+);
+
+SELECT * FROM audicion
+  
+CREATE TABLE X
+(
+	edad INT NULL,
+	audifonos INT NULL,
+    	frecuencia INT NULL,
+		horas INT NULL,
+		traumas INT NULL,
+		necesidad INT NULL,
+		repetir INT NULL,
+		ruidofondo INT NULL,
+		timbre INT NULL,
+		entornosruidosos INT NULL,
+		
+);
+
+INSERT x
+(edad,audifonos,frecuencia,horas,traumas,necesidad,repetir,ruidofondo,timbre,entornosruidosos)
+SELECT edad,audifonos,frecuencia,horas,traumas,necesidad,repetir,ruidofondo,timbre,entornosruidosos FROM audicion
+WHERE edad>1
+
+ SELECT * FROM X
+    
+
+CREATE TABLE Y
+(
+	
+		segundotest INT NULL,
+		
+);
+
+INSERT Y
+(segundotest)
+SELECT segundotest FROM audicion
+WHERE edad>1
+
+
+
+SELECT * FROM Y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
